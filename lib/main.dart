c7cf4f2158111d1384af928f54f78a1d965737d1
alaminXpro <alaminxpro@gmail.com>
2023-12-04 14:23:22 +0600

    Commit on branch 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bookstore/pages/HomePage.dart';
+import 'package:bookstore/pages/ItemPage.dart';
 import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
         ),
         routes: {
           "/": (context) => HomePage(),
+          "ItemPage": (context) => ItemPage(),
         }
     );
   }
